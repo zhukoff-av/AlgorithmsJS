c802af54bbd62299895674f0eb78b6fb782d94f5
zhukoff-av <mr.zhukoff@gmail.com>
2024-03-05 08:24:53 +0100

    fix leetcode 200.

--- src/leetcode/01.UnionFind/200.NrifIslands.js
+++ src/leetcode/01.UnionFind/200.NrifIslands.js
@@ -1,65 +1,97 @@
-const numOfIslands = (grid) => {
-    const parent = Array(grid).fill(null)
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const numOfIslands = function (grid) {
+    const m = grid.length
+    if (m === 0) return 0
+    const n = grid[0].length
 
-    for (let i = 0; i < grid.length; i++) {
-        parent[i] = i
-    }
+    const parent = Array(m * n).fill(-1).map((_, index) => index)
+
     const find = (node) => {
         if (parent[node] !== node) {
-            node = find(parent[node])
+            parent[node] = find(parent[node]);
         }
-        return node
+        return parent[node];
     }
 
     const union = (p, q) => {
         const root1 = find(p)
         const root2 = find(q)
 
-        if (parent[root2] !== parent[root1]) {
-            parent[root2] = root1
+        if (root1 !== root2) {
+            parent[root2] = root1;
         }
     }
 
-    const m = grid.length
-    const n = grid[0].length
-
-    const direction = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (grid[i][j] === '1') {
-                for (const [dx, dy] of direction) {
+                for (let [dx, dy] of directions) {
                     const newX = i + dx
                     const newY = j + dy
+
                     if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
                         if (grid[newX][newY] === '1') {
                             union(i * n + j, newX * n + newY)
                         }
                     }
+
                 }
             }
-
         }
-
     }
 
-    // counter
-    // for p[i] === i -> ++
-    let counter = 0
-    for (let i = 0; i < m * n; i++) {
-        if (find(i) === i) {
-            counter++
+    let counter = 0;
+    const islands = new Set();
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1') {
+                const root = find(i * n + j);
+                if (!islands.has(root)) {
+                    counter++;
+                    islands.add(root);
+                }
+            }
         }
-
     }
 
-    return counter
-
-
-}
+    return counter;
+};
 
 const testCases = [
+    {name: 'Single element grid', input: [['0']], expected: 0},
+    {name: '0, 0', input: [['0', '0']], expected: 0},
+    {name: '0, 1', input: [['0', '1']], expected: 1},
+    {name: '1, 0', input: [['1', '0']], expected: 1},
+    {name: '1, 1', input: [['1', '1']], expected: 1},
+    {name: '1, 0, 1', input: [['1', '0', '1']], expected: 2},
+    {name: '1, 1, 0, 1', input: [['1', '1', '0', '1']], expected: 2},
+    {name: '1, 1, 0, 1, 1, 1, 0', input: [['1', '1', '0', '1', '1', '1', '0']], expected: 2},
+    {name: '1, 1, 0, 1, 0, 1, 0', input: [['1', '1', '0', '1', '0', '1', '0']], expected: 3},
+    {
+        name: 'grid 1 island',
+        input: [
+            ['1', '1', '1', '1', '0'],
+            ['1', '1', '0', '1', '0'],
+            ['1', '1', '0', '0', '0'],
+            ['0', '0', '0', '0', '0'],
+        ],
+        expected: 1,
+    },
+    {
+        name: 'grid 3 islands',
+        input: [
+            ['1', '1', '0', '0', '0'],
+            ['1', '1', '0', '0', '0'],
+            ['0', '0', '1', '0', '0'],
+            ['0', '0', '0', '1', '1'],
+        ],
+        expected: 3,
+    },
     {
         name: 'grid 1 island', input: [
             ["1", "1", "1", "1", "0"],
