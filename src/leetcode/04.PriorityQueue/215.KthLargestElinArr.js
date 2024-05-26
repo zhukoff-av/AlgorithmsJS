e3ee5b52d82ab5e94ddcef56364921d3b27ddcf9
zhukoff-av <mr.zhukoff@gmail.com>
2024-05-26 17:05:30 +0200

    lc 215.

---
+++ src/leetcode/04.PriorityQueue/215.KthLargestElinArr.js
@@ -0,0 +1,80 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    getParentIndex(i) {
+        return Math.floor((i - 1) / 2);
+    }
+
+    getLeftChildIndex(i) {
+        return 2 * i + 1;
+    }
+
+    getRightChildIndex(i) {
+        return 2 * i + 2;
+    }
+
+    peek() {
+        if (!this.heap.length) return null
+        return this.heap[0]
+    }
+
+    size() {
+        return this.heap.length
+    }
+
+    swap(i, j) {
+        const temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0 && this.heap[index] < this.heap[this.getParentIndex(index)]) {
+            this.swap(index, this.getParentIndex(index));
+            index = this.getParentIndex(index);
+        }
+    }
+
+    extractMin() {
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleDown() {
+        let index = 0;
+        while (this.getLeftChildIndex(index) < this.heap.length) {
+            let smallerChildIndex = this.getLeftChildIndex(index);
+            if (this.getRightChildIndex(index) < this.heap.length && this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]) {
+                smallerChildIndex = this.getRightChildIndex(index);
+            }
+            if (this.heap[index] < this.heap[smallerChildIndex]) {
+                break;
+            }
+            this.swap(index, smallerChildIndex);
+            index = smallerChildIndex;
+        }
+    }
+}
+
+const kthLargestEl = (nums, k) => {
+    const minHeap = new MinHeap()
+    for (let num of nums) {
+        minHeap.insert(num)
+        if(minHeap.size() > k){
+            minHeap.extractMin()
+        }
+    }
+    return minHeap.peek()
+}
+
+console.log(kthLargestEl([3,2,1,5,6,4], 2))
