403809b5a9044d69b5d587603ba67859cf1cc4a6
zhukoff-av <mr.zhukoff@gmail.com>
2024-06-06 21:35:56 +0200

    lc 1424

---
+++ src/leetcode/04.PriorityQueue/1424DiagonalTraverse.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+const findDiagonalOrder = (nums) => {
+
+    let result = []
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums[0].length; j++) {
+            const num = nums[i][j]
+            if (!num) continue
+            const diagonal = result[i + j]
+            diagonal ? diagonal.unshift(num) : result[i + j] = [num]
+        }
+    }
+    console.log(result.flat())
+    return result.flat()
+};
+
+
+findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
