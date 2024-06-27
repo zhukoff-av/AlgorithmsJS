a1ffde97b4b5b3b6beb5e260e242f3c3b5a798c0
zhukoff-av <mr.zhukoff@gmail.com>
2024-06-27 21:23:06 +0200

    lc 658. second solution

--- src/leetcode/04.PriorityQueue/658.FindClosestEl.js
+++ src/leetcode/04.PriorityQueue/658.FindClosestEl.js
@@ -20,4 +20,15 @@ var findClosestElements = function(arr, k, x) {
     }
 
     return ans.sort((a,b)=> a-b);
+
+
+    // second solution Two Pointer. More efficient O(n)
+    // let leftPointer = 0;
+    // let rightPointer = arr.length - 1;
+    // while (rightPointer - leftPointer >= k) {
+    //     // a-x <= b-x (Comparison Law at the Question)
+    //     if (Math.abs(arr[leftPointer] - x) <= Math.abs(arr[rightPointer] - x)) rightPointer--;
+    //     else leftPointer++;
+    // }
+    // return arr.slice(leftPointer, rightPointer + 1);
 };
