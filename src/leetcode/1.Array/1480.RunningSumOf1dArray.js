a12bcf0f228845d49c4654ef32b6e156cabbcfb0
zhukoff-av <mr.zhukoff@gmail.com>
2024-07-16 08:36:22 +0200

    array 1480.RunningSumOf1dArray.js

---
+++ src/leetcode/1.Array/1480.RunningSumOf1dArray.js
@@ -0,0 +1,22 @@
+
+
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+
+
+const sum = (nums) => {
+
+    let result = []
+    let acc = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        acc += nums[i]
+
+        result.push(acc)
+    }
+
+    return result
+}
+
+console.log(sum([1,2,3,4]))
