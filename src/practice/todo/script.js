9fdbb5773f3a90aa10c5c4d3d267ed673924e1d5
zhukoff-av <mr.zhukoff@gmail.com>
2024-11-19 21:52:08 +0100

    small fixes

--- src/practice/todo/script.js
+++ src/practice/todo/script.js
@@ -1,38 +1,37 @@
-var list = document.querySelector('.todo-list');
-var items = list.children;
-var emptyListMessage = document.querySelector('.empty-tasks');
-var newItemForm = document.querySelector('.add-form');
-var newItemTitle = newItemForm.querySelector('.add-form-input');
-var taskTemplate = document.querySelector('#task-template').content;
-var newItemTemplate = taskTemplate.querySelector('.todo-list-item');
+const list = document.querySelector('.todo-list');
+const items = list.children;
+const emptyListMessage = document.querySelector('.empty-tasks');
+const newItemForm = document.querySelector('.add-form');
+const newItemTitle = newItemForm.querySelector('.add-form-input');
+const taskTemplate = document.querySelector('#task-template').content;
+const newItemTemplate = taskTemplate.querySelector('.todo-list-item');
+const input = document.querySelector('.add-form-input');
 
-var toggleEmptyListMessage = function () {
+const toggleEmptyListMessage = function () {
     if (items.length === 0) {
         emptyListMessage.classList.remove('hidden');
     }
 };
 
-var addCheckHandler = function (item) {
-    var checkbox = item.querySelector('.todo-list-input');
+const addCheckHandler = function (item) {
+    const checkbox = item.querySelector('.todo-list-input');
     checkbox.addEventListener('change', function () {
         item.remove();
         toggleEmptyListMessage();
     });
 };
 
-for (var i = 0; i < items.length; i++) {
+for (let i = 0; i < items.length; i++) {
     addCheckHandler(items[i]);
 }
 
 newItemForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
-
-    var taskText = newItemTitle.value;
-    var task = newItemTemplate.cloneNode(true);
-    var taskDescription = task.querySelector('span');
+    const taskText = newItemTitle.value;
+    const task = newItemTemplate.cloneNode(true);
+    const taskDescription = task.querySelector('span');
     taskDescription.textContent = taskText;
-
-    // Добавьте вызов функции addCheckHandler сюда
     addCheckHandler(task)
     list.appendChild(task);
+    input.value = '';
 });
